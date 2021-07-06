782b5a83f16d032e002af0da3e68b76f49ffbfe5
Nataliia <nata.buchna@gmail.com>
2021-07-06 11:47:16 +0300

    4

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log('------------- # 4')
+
+function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+}
+ask(
+    "Вы согласны?",
+    () => console.log("Вы согласились."),
+    () => console.log("Вы отменили выполнение.")
+);
+
